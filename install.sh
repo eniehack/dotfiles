bec745be4fc48516a8cb53e9679a55d62751d8ee
eniehack <eniehack@outlook.jp>
2020-03-01 13:18:10 +0900

    delete if sentence.

--- install.sh
+++ install.sh
@@ -2,20 +2,14 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-if [ ! -e ${XDG_CONFIG_HOME}/nvim ]; then
-	mkdir ${XDG_CONFIG_HOME}/nvim/
-fi
+which fish || echo "no fish"
 
-which python
-if [ ! $? = 0 ]; then
-	sudo pacman -S python
-fi
+fc-list : family style | grep HackGen || echo "no HackGen Font"
 
-pip freeze | grep -q neovim
-if [ ! $? = 0 ]; then
-	pip install --user neovim
-fi
-	
+-e ${XDG_CONFIG_HOME}/nvim || export $XDG_CONFIG_HOME="$HOME/.config" && mkdir ${XDG_CONFIG_HOME}/nvim/
+
+which python || sudo pacman -S python
+pip freeze | grep -q neovim || pip install --user neovim
 
 ln -sf ${SCRIPT_DIR}/neovim/init.vim ${XDG_CONFIG_HOME}/nvim/init.vim
 ln -sf ${SCRIPT_DIR}/neovim/dein.toml ~/dein.toml
