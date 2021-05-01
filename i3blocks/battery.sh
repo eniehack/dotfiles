d2c93c406908e36d91c306343b419aa5b60208b9
eniehack <eniehack@outlook.jp>
2021-05-01 14:31:34 +0900

    [i3blocks] add emoji support

--- i3blocks/battery.sh
+++ i3blocks/battery.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-ACPI=$(acpi -b)
-BAT=$(echo $ACPI | awk -F'[,]' '{print $2}' | sed -r "s/([0-9]+)%,/\1/g")
-CHARGING=$(echo $ACPI | awk -F'[: ,]' '{print $4}')
+BAT=$(acpi -b | awk -F'[,]' '{print $2}' | sed -r "s/%//g")
+CHARGING=$(acpi -b | awk -F'[: ,]' '{print $4}')
 
-echo -e "\uf243$BAT ($CHARGING)"
+if [ "$CHARGING" = "Charging" ]; then
+    echo -e "\uf0e7$BAT%"
+elif [ "$BAT" -le 10 ]; then
+    echo -e "\uf582$BAT%"
+elif [ "$BAT" -le 20 ]; then
+    echo -e "\uf57a$BAT%"
+elif [ "$BAT" -le 50 ]; then
+    echo -e "\uf57d$BAT%"
+elif [ "$BAT" -le 75 ]; then
+    echo -e "\uf580$BAT%"
+else
+    echo -e "\uf578$BAT%"
+fi
 
-if [ $BAT -le 5] ; then
+if [ "$BAT" -le 5 ]; then
     echo "#ff0000"
-elif [ $BAT -le 20] ; then
+elif [ "$BAT" -le 20 ]; then
     echo "#ff8c00"
 else
     echo "#008000"
--- i3blocks/network.sh
+++ i3blocks/network.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
-if [[ -z $INTERFACE ]] ; then
+if [ -z $INTERFACE ]; then
     INTERFACE=$(ip route | awk '/^default/ {print $5; exit}')
 fi
 
-[[ -z $INTERFACE ]] && exit
+[ -z $INTERFACE ] && exit
 
-if [[ -n "$(iw dev $INTERFACE link | awk '{ print $1 }' | grep -E -o 'Not')" ]] ; then
-    echo -e "\uf1eb disconnect"
+if [ -n "$(iw dev $INTERFACE link | awk '{ print $1 }' | grep -E -o 'Not')" ] ; then
+    echo -e "\ufaa9 disconnect"
     echo "#ff0000"
 else
-    echo -e "\uf1eb $(iw dev $INTERFACE link | awk '/SSID:/ {print $2}')"
+    echo -e "\ufaa8 $(iw dev $INTERFACE link | awk '/SSID:/ {print $2}')"
     echo "#008800"
 fi
 
