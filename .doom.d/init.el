29527de3e58ba5aba23f40820b99bcd4f3c8d61e
Nakaya <eniehack@outlook.jp>
2022-09-04 12:54:39 +0900

    doom: tidy

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -111,8 +111,9 @@
 
        :lang
        ;;agda              ; types of types of types of types...
+       ;;beancount
        (cc +lsp)                ; C/C++/Obj-C madness
-       ;;clojure           ; java with a lisp
+       clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        crystal           ; ruby at the speed of c
@@ -128,7 +129,7 @@
        (fsharp +lsp)            ; ML stands for Microsoft's Language
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
-       ;;(go +lsp)         ; the hipster dialect
+       (go +lsp)         ; the hipster dialect
        ;;(haskell +dante)  ; a language that's lazier than I am
        hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
@@ -156,7 +157,7 @@
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
-       ;; (ruby +lsp)   ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       (ruby +lsp)   ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        (rust +lsp)   ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        (scheme +racket +chicken +gauche)            ; a fully conniving family of lisps
