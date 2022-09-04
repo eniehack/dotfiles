051a046cb8db7bb86157e694a858b9801b580817
Nakaya <eniehack@outlook.jp>
2022-09-04 12:56:46 +0900

    use pactl instead of amixer

--- .config/i3blocks/volume.sh
+++ .config/i3blocks/volume.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-ALSA=$(amixer -c 0 get Master | awk '/Mono: / {print $4 $6}' | sed 's/]\[/\ /g' | sed 's/\(\[\|\]\)//g')
-VOL=$(echo "$ALSA" | awk '{print $1}')
-MUTE=$(echo "$ALSA" | awk '{print $2}')
+#ALSA=$(amixer -c 0 get Master | awk '/Mono: / {print $4 $6}' | sed 's/]\[/\ /g' | sed 's/\(\[\|\]\)//g')
+VOL=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '/Volume: / { print $5}')
+#VOL=$(echo "$ALSA" | awk '{print $1}')
+MUTE=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '/Mute: / { print $2 }')
+#MUTE=$(echo "$ALSA" | awk '{print $2}')
+
+echo -e "\uf485$VOL"
 
 if [ $MUTE = "on" ]; then
-    echo -e "\uf485$VOL"
-else
-    echo -e "\uf466$VOL"
+    echo "#6272a4"
 fi
+
+exit 0
