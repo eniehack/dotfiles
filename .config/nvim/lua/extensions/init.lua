307acd6ded9ee82f1972418ecbf5a634a41217e5
Nakaya <eniehack@outlook.jp>
2024-02-01 04:30:38 +0900

    nvim: lsp

--- .config/nvim/lua/extensions/init.lua
+++ .config/nvim/lua/extensions/init.lua
@@ -2,38 +2,44 @@ return {
     {
         'vim-jp/vimdoc-ja',
         lazy = true,
+        keys = {
+            { "h", mode = "c" },
+        },
     },
     {
         'nvim-treesitter/nvim-treesitter',
         event = "BufReadPost",
         build = ":TSUpdate",
         opts = {
-      	    ensure_installed = {
-      	      "bash",
-      	      "html",
-      	      "javascript",
-      	      "json",
-      	      "lua",
-      	      "markdown",
-      	      "markdown_inline",
-      	      "python",
-      	      "regex",
-      	      "tsx",
-      	      "typescript",
-      	      "vim",
-    	    },
-    	},
+            ensure_installed = {
+                "bash",
+                "html",
+                "javascript",
+                "json",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "regex",
+                "tsx",
+                "typescript",
+                "vim",
+                "svelte",
+            },
+        },
     },
     {
         'RRethy/nvim-base16',
-        event = "BufEnter",
+        -- event = "BufEnter",
+        lazy = true,
         config = function()
             vim.cmd("colorscheme base16-dracula")
         end
     },
     {
         'nvim-lualine/lualine.nvim',
-        event = "BufEnter",
+        --event = "BufEnter",
+        event = "VimEnter",
         lazy = true,
         dependencies = {
             'nvim-tree/nvim-web-devicons',
@@ -46,10 +52,10 @@ return {
                 sections = {
                     lualine_c = {
                         {
-                          'diagnostics',
-                          sources = { 'nvim_diagnostic', 'nvim_lsp' },
-                          sections = { 'error', 'warn', 'info', 'hint' },
-                          symbols = { error = ' ', warn = ' ', info = ' ', hint = '' },
+                            'diagnostics',
+                            sources = { 'nvim_diagnostic', 'nvim_lsp' },
+                            sections = { 'error', 'warn', 'info', 'hint' },
+                            symbols = { error = ' ', warn = ' ', info = ' ', hint = '' },
                         },
                     },
                 },
@@ -62,38 +68,64 @@ return {
         'tpope/vim-fugitive',
         lazy = true,
         keys = {
-            {"<leader>gg", "<cmd>Git<CR>", desc = "git status"},
-            {"<leader>gc", "<cmd>Git commit<CR>", desc = "git commit"},
+            { "<leader>gg", "<cmd>Git<CR>",        desc = "git status" },
+            { "<leader>gc", "<cmd>Git commit<CR>", desc = "git commit" },
         }
     },
     {
         "folke/which-key.nvim",
-        event = "BufReadPost",
+        event = "BufEnter",
         config = function()
             vim.o.timeout = true
             vim.o.timeoutlen = 300
             require("which-key").setup {
-              -- your configuration comes here
-              -- or leave it empty to use the default settings
-              -- refer to the configuration section below
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
             }
         end
     },
 
     -- file manager
     {
-        'junegunn/fzf.vim',
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.4',
+        event = "BufEnter",
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-treesitter/nvim-treesitter',
+            'nvim-tree/nvim-web-devicons',
+        },
         keys = {
-            {"<leader>of", "<cmd>Files .<CR>", desc = "files list"},
-            {"<leader>bl", "<cmd>Buffers .<CR>", desc = "buffers list"},
-        }
+            { "<leader>ff", function() require("telescope.builtin").find_files(require("telescope.themes").get_ivy()) end, desc = "find files" },
+            { "<leader>bf", function() require("telescope.builtin").buffers(require("telescope.themes").get_ivy()) end, desc = "find buffers" },
+        },
+        cmd = {
+            "Telescope",
+        },
     },
+    -- {
+    --     'junegunn/fzf.vim',
+    --     lazy = true,
+    --     keys = {
+    --         {"<leader>of", "<cmd>Files .<CR>", desc = "files list"},
+    --         {"<leader>bl", "<cmd>Buffers .<CR>", desc = "buffers list"},
+    --     },
+    --     cmd = {
+    --         "Files",
+    --         "Buffers",
+    --     },
+    --     config = function()
+    --         require("fzf").setup()
+    --     end
+    -- },
     {
         "lambdalisue/fern.vim",
+        event = "BufEnter",
         keys = {
-            {"<leader>op", "<cmd>Fern . -drawer<CR>", desc = "fern on cwd"},
+            { "<leader>fd", "<cmd>Fern . -drawer<CR>", desc = "fern on cwd" },
         },
-        cmd = "Fern",
+        cmd = "Fern"
     },
 
     -- utils
@@ -103,41 +135,63 @@ return {
     },
     {
         'mattn/emmet-vim',
-        ft = {'html', 'elm', 'gohtmltmpl', 'php'}
+        ft = { 'html', 'elm', 'gohtmltmpl', 'php', "svelte" },
     },
 
     -- completion
     {
         -- https://coralpink.github.io/commentary/neovim/lsp/nvim-cmp.html
         'hrsh7th/nvim-cmp',
-        event = "InsertEnter",
+        event = {"InsertEnter", "CmdlineEnter"},
         dependencies = {
             'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-vsnip',
+            "hrsh7th/cmp-buffer",
         },
         config = function()
             local cmp = require 'cmp'
             local map = cmp.mapping
 
             cmp.setup {
-              mapping = map.preset.insert {
-                ['<C-d>'] = map.scroll_docs(-4),
-                ['<C-f>'] = map.scroll_docs(4),
-                ['<C-Space>'] = map.complete(),
-                ['<C-e>'] = map.abort(),
-                ['<CR>'] = map.confirm { select = false },
-              },
-              sources = cmp.config.sources {
-                { name = 'nvim_lsp' },
-              },
+                mapping = map.preset.insert {
+                    ["<C-p>"] = cmp.mapping.select_prev_item(),
+                    ["<C-n>"] = cmp.mapping.select_next_item(),
+                    ['<C-d>'] = map.scroll_docs(-4),
+                    ['<C-f>'] = map.scroll_docs(4),
+                    ['<C-Space>'] = map.complete(),
+                    ['<C-e>'] = map.abort(),
+                    ['<CR>'] = map.confirm {
+                        behavior = cmp.ConfirmBehavior.Replace,
+                        select = true,
+                    },
+                },
+                sources = cmp.config.sources {
+                    { name = 'nvim_lsp' },
+                    { name = 'vsnip' },
+                    { name = 'buffer' },
+                },
+                snippet = {
+                    expand = function(args)
+                        vim.fn["vsnip#anonymous"](args.body)
+                    end,
+                }
             }
         end
+    },
+    {
+        'hrsh7th/cmp-vsnip',
+        lazy = true,
+        dependencies = {
+            'hrsh7th/vim-vsnip',
+            'hrsh7th/vim-vsnip-integ',
+        },
     },
 
     -- lsp
     {
         "neovim/nvim-lspconfig",
         lazy = true,
-        config = function ()
+        config = function()
             require "extensions.nvim-lspconfig"
         end,
     },
@@ -145,7 +199,7 @@ return {
         -- https://coralpink.github.io/commentary/neovim/lsp/mason.html
         "williamboman/mason.nvim",
         --event = "InsertEnter",
-      	event = "BufReadPost",
+        event = "BufReadPost",
         build = ":MasonUpdate",
         cmd = "Mason",
         dependencies = {
@@ -158,24 +212,34 @@ return {
         end
     },
     {
-      	"williamboman/mason-lspconfig.nvim",
-      	lazy = true,
-      	--event = "InsertEnter",
-      	dependencies = {
-      	      "neovim/nvim-lspconfig",
-      	      'hrsh7th/cmp-nvim-lsp',
-      	  --{ "echasnovski/mini.completion", version = false },
-      	},
-      	config = function()
+        "williamboman/mason-lspconfig.nvim",
+        lazy = true,
+        --event = "InsertEnter",
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            'hrsh7th/cmp-nvim-lsp',
+            --{ "echasnovski/mini.completion", version = false },
+        },
+        config = function()
             local lspconfig = require("lspconfig")
             --require('mini.completion').setup({})
             require("mason-lspconfig").setup_handlers {
-              function(server_name)
-                lspconfig[server_name].setup {
-                    capabilities = require('cmp_nvim_lsp').default_capabilities(),
-                }
-              end,
+                function(server_name)
+                    lspconfig[server_name].setup {
+                        capabilities = require('cmp_nvim_lsp').default_capabilities(),
+                    }
+                end,
             }
-	end
+        end
+    },
+    -- go
+    {
+        'fatih/vim-go',
+        ft = { "go" },
+    },
+    -- rust
+    {
+        "rust-lang/rust.vim",
+        ft = { "rust" },
     }
 }
--- .config/nvim/lua/extensions/nvim-lspconfig.lua
+++ .config/nvim/lua/extensions/nvim-lspconfig.lua
@@ -1,7 +1,7 @@
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>le', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+vim.keymap.set('n', '<leader>lq', vim.diagnostic.setloclist)
 vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(ev)
         -- Enable completion triggered by <c-x><c-o>
@@ -11,22 +11,22 @@ vim.api.nvim_create_autocmd("LspAttach", {
         -- See `:help vim.lsp.*` for documentation on any of the below functions
         local opts = { buffer = ev.buf }
 
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wl', function()
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {unpack(opts), desc = "declaration"})
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {unpack(opts), desc = "definition"})
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {unpack(opts), desc = "hover"})
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {unpack(opts), desc = "implementation"})
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, {unpack(opts), desc = "signature"})
+        vim.keymap.set('n', '<leader>lwa', vim.lsp.buf.add_workspace_folder, {unpack(opts), desc = "add workspace"})
+        vim.keymap.set('n', '<leader>lwr', vim.lsp.buf.remove_workspace_folder, {unpack(opts), desc = "remove workspace"})
+        vim.keymap.set('n', '<leader>lwl', function()
           print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, opts)
-        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<space>f', function()
+        end,{unpack(opts), desc = "list workspace"} )
+        vim.keymap.set('n', '<leader>lD', vim.lsp.buf.type_definition, {unpack(opts), desc = "type definition"})
+        vim.keymap.set('n', '<leader>lrn', vim.lsp.buf.rename, {unpack(opts), desc = "rename"})
+        vim.keymap.set('n', '<leader>lca', vim.lsp.buf.code_action, {unpack(opts), desc = "code action"})
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, {unpack(opts), desc = "show references"})
+        vim.keymap.set('n', '<leader>lf', function()
           vim.lsp.buf.format { async = true }
-        end, opts)
+        end, {unpack(opts), desc = "format"})
     end,
 })
